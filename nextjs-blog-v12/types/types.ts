0a4c828e7715d102d55479a20c3ebfcaaf0dab0e
HiImConan <mindol0320@naver.com>
2023-07-17 13:46:56 +0900

    docs: update post type detailed

--- nextjs-blog-v12/types/types.ts
+++ nextjs-blog-v12/types/types.ts
@@ -10,3 +10,18 @@ export interface PostInfo extends Metadata {
 export interface PostData extends PostInfo {
   contentHtml: string;
 }
+
+export type PostMatter = {
+  title: string;
+  description: string;
+  tags: string[];
+  draft?: boolean;
+  date: string;
+};
+
+export type Post = PostMatter & {
+  slug: string;
+  content: string;
+  readingMinutes: number;
+  wordCount: number;
+};
