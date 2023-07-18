6449aab632e1411945a6aba61fc26b000c0cc1af
HiImConan <mindol0320@naver.com>
2023-07-18 21:49:04 +0900

    chore: add mutiple rehype/remark plugins

--- nextjs-blog-v12/next.config.js
+++ nextjs-blog-v12/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
