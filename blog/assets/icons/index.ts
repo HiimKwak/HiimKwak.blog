d38c3b6819ee8b3040006b3955feb40467baba71
HiImConan <mindol0320@naver.com>
2023-09-20 23:10:52 +0900

    Edit folder name: nextjs-blog-v12 to blog

---
+++ blog/assets/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as IcCalendar } from "./IcCalendar";
+export { default as IcClock } from "./IcClock";
---
+++ blog/contentlayer.config.ts
@@ -0,0 +1,73 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import readingTime from "reading-time";
+import path from "path";
+// Remark packages
+import remarkGfm from "remark-gfm";
+import remarkFootnotes from "remark-footnotes";
+import remarkMath from "remark-math";
+import remarkExtractFrontmatter from "./libs/remark-extract-frontmatter";
+import remarkCodeTitles from "./libs/remark-code-title";
+import { extractTocHeadings } from "./libs/remark-toc-headings";
+import remarkImgToJsx from "./libs/remark-img-to-jsx";
+// Rehype packages
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeKatex from "rehype-katex";
+import rehypeCitation from "rehype-citation";
+import rehypePrismPlus from "rehype-prism-plus";
+import rehypePresetMinify from "rehype-preset-minify";
+
+const root = process.cwd();
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    icon: { type: "string" },
+    image: { type: "string" },
+    tags: { type: "list", of: { type: "string" }, required: true },
+    draft: { type: "boolean" },
+    date: { type: "string", required: true },
+  },
+  computedFields: {
+    readingTime: {
+      type: "number",
+      resolve: (post) => Math.ceil(readingTime(post.body.raw).minutes),
+    },
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+    toc: {
+      type: "string",
+      resolve: (post) => extractTocHeadings(post.body.raw),
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "posts",
+  documentTypes: [Post],
+  mdx: {
+    cwd: process.cwd(),
+    remarkPlugins: [
+      remarkExtractFrontmatter,
+      remarkGfm,
+      remarkCodeTitles,
+      [remarkFootnotes, { inlineNotes: true }],
+      remarkMath,
+      remarkImgToJsx,
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+      rehypeKatex,
+      [rehypeCitation, { path: path.join(root, "data") }],
+      [rehypePrismPlus, { ignoreMissing: true }],
+      rehypePresetMinify,
+    ],
+  },
+});
