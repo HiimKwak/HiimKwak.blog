1770cab5641f89c0cd2dabd5bf676a84ab3dd1c0
HiImConan <mindol0320@naver.com>
2023-09-21 13:24:09 +0900

    Add next export optimize images and configure options

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,11 +1,12 @@
 const { withContentlayer } = require("next-contentlayer");
+const withExportImages = require("next-export-optimize-images");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withExportImages({
   reactStrictMode: true,
   // Configure pageExtensions to include md and mdx
   swcMinify: true,
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-};
+});
 
 module.exports = withContentlayer(nextConfig);
