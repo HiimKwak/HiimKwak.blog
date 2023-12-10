e43803ed208416c9e5ebd42c0e37802f176cb737
HiImConan <mindol0320@naver.com>
2023-12-10 22:43:12 +0900

    1.3.1: 벨로그 포스트 이전, 모바일 뷰 h1태그 title -> break-keep 변경

--- blog/app/post/[slug]/page.tsx
+++ blog/app/post/[slug]/page.tsx
@@ -90,7 +90,7 @@ export default function Post({ params }: { params: { slug: string } }) {
 
   return (
     <section>
-      <h1 className='title font-medium text-2xl tracking-tighter max-w-[650px]'>
+      <h1 className='font-medium text-2xl tracking-tighter max-w-[650px] break-keep'>
         {post.metadata.title}
       </h1>
       <div className='flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]'>
