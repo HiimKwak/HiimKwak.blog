a1f13bfe1e305d2ef8b6d2ae7c8af191408eefca
hiimkwak <mindol0320@naver.com>
2024-11-20 08:51:38 +0900

    feat: 네브바 수정, 탭 추가

--- blog/app/components/layout/nav.tsx
+++ blog/app/components/layout/nav.tsx
@@ -1,26 +1,40 @@
-'use client';
+"use client";
 
-import { motion, LayoutGroup } from 'framer-motion';
-import { Suspense, useEffect } from 'react';
+import { motion, LayoutGroup } from "framer-motion";
+import { Suspense, useEffect } from "react";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-import useIntersectionObserver from 'app/hooks/useIntersectionObserver';
-import { $ } from 'app/libs/core';
+import useIntersectionObserver from "app/hooks/useIntersectionObserver";
+import { $ } from "app/libs/core";
+import Image from "next/image";
 
 const navItems = {
-  '/': {
-    name: '홈',
+  left: {
+    "/": {
+      name: "홈",
+    },
+    "/post": {
+      name: "포스트",
+    },
+    "/series": {
+      name: "시리즈",
+    },
   },
-  '/post': {
-    name: '포스트',
+  right: {
+    "/about": {
+      name: "프로필",
+    },
+    "/github": {
+      name: "github",
+    },
   },
 };
 
 export default function Navbar() {
   useEffect(() => {
-    const target = document.getElementById('header-flag');
+    const target = document.getElementById("header-flag");
     setTargetElement(target);
   }, []);
 
@@ -29,22 +43,29 @@ export default function Navbar() {
   return (
     <aside
       className={$(
-        '-ml-[8px] tracking-tight px-2 py-4 md:px-0',
+        "-ml-[8px] tracking-tight px-2 py-4 md:px-0",
         !isIntersected &&
-          'bg-transparent border-b border-neutral-300 dark:border-gray-600 backdrop-blur-xl'
+          "bg-transparent border-b border-neutral-300 dark:border-gray-600 backdrop-blur-xl"
       )}
     >
-      <div className='max-w-2xl md:mx-auto'>
+      <div className="max-w-2xl md:mx-auto">
         <LayoutGroup>
           <nav
-            className='relative flex flex-row items-start px-4 pb-0 fade md:overflow-auto scroll-pr-6 md:relative md:px-0'
-            id='nav'
+            className="flex px-4 pb-0 fade md:overflow-auto scroll-pr-6 md:relative md:px-0"
+            id="nav"
           >
-            <div className='flex flex-row pr-10 space-x-0'>
+            <div className="flex w-full justify-between">
               <Suspense fallback={null}>
-                {Object.entries(navItems).map(([path, { name }]) => {
-                  return <NavItem key={path} path={path} name={name} />;
-                })}
+                <div className="flex space-x-0">
+                  {Object.entries(navItems.left).map(([path, { name }]) => (
+                    <NavItem key={path} path={path} name={name} />
+                  ))}
+                </div>
+                <div className="flex space-x-0">
+                  {Object.entries(navItems.right).map(([path, { name }]) => (
+                    <NavItem key={path} path={path} name={name} />
+                  ))}
+                </div>
               </Suspense>
             </div>
           </nav>
@@ -55,31 +76,44 @@ export default function Navbar() {
 }
 
 function NavItem({ path, name }: { path: string; name: string }) {
-  let pathname = usePathname() || '/';
-  if (pathname.includes('/post/')) {
-    pathname = '/post';
+  let pathname = usePathname() || "/";
+  if (pathname.includes("/post/")) {
+    pathname = "/post";
   }
   let isActive = path === pathname;
+
+  let isGithub = path === "/github";
+  const linkPath = isGithub ? "https://github.com/HiimKwak" : path;
 
   return (
     <Link
       key={path}
-      href={path}
+      href={linkPath}
       className={$(
-        'transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle',
+        "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle",
         {
-          'text-neutral-500': !isActive,
+          "text-neutral-500": !isActive,
         }
       )}
     >
-      <span className='relative px-2 py-1'>
-        {name}
+      <span className="relative px-2 py-1 text-center flex items-center">
+        {isGithub ? (
+          <Image
+            alt="github-icon"
+            src="/github-mark.svg"
+            className="h-[14px] w-[14px]"
+            width="14"
+            height="14"
+          />
+        ) : (
+          name
+        )}
         {path === pathname ? (
           <motion.div
-            className='absolute h-[1px] top-7 mx-2 inset-0 bg-sunglow dark:bg-gradient-to-r from-transparent to-sunglow'
-            layoutId='sidebar'
+            className="absolute h-[1px] top-7 mx-2 inset-0 bg-sunglow dark:bg-gradient-to-r from-transparent to-sunglow"
+            layoutId="sidebar"
             transition={{
-              type: 'spring',
+              type: "spring",
               stiffness: 350,
               damping: 30,
             }}
