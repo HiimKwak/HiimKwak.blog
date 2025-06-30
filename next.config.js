7ff883673f080d28d6f2665da1a75421fab754fb
hiimkwak <mindol0320@naver.com>
2025-06-30 15:50:06 +0900

    vercel function이 fs 파일 못읽어오는 이슈

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  outputFileTracingIncludes: {
+    '/post/[slug]': ['./content_publish/**/*']
+  }
 };
 
 module.exports = nextConfig;
