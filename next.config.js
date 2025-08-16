77bf80b702f9399bb0852470968cca83982fe309
hiimkwak <mindol0320@naver.com>
2025-08-17 08:52:29 +0900

    build: vercelignore 추가, next config에서 notes도 content_publish 추가

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
 	// Configure pageExtensions to include md and mdx
 	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 	outputFileTracingIncludes: {
 		"/post/[slug]": ["./content_publish/**/*"],
+		"/notes/[...slug]": ["./content_publish/**/*"],
 	},
 };
 
