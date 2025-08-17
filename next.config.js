1e65f7854266db3ef809af5ae27cf42a00790aff
hiimkwak <mindol0320@naver.com>
2025-08-17 10:55:00 +0900

    test: pnpm store만 제외

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	// Configure pageExtensions to include md and mdx
 	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 	outputFileTracingExcludes: {
-		"/*": ["./content_publish/**/*", ".pnpm-store/v3/files"],
+		"/*": [".pnpm-store/v3/files"],
 	},
 };
 
