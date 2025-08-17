dbc1da08976e5df26cb1c23c892f6db6ab422e8f
hiimkwak <mindol0320@naver.com>
2025-08-17 10:40:41 +0900

    build: exclude output file

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
 	// Configure pageExtensions to include md and mdx
 	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 	outputFileTracingExcludes: {
-		"/post/*": ["./content_publish/**/*"],
-		"/notes/*": ["./content_publish/**/*"]
+		"/*": ["./content_publish/**/*", ".pnpm-store/v3/files"],
 	},
 };
 
