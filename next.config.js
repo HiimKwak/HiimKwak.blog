83cb896b49f7a584637333bfad26decbc196f70a
hiimkwak <mindol0320@naver.com>
2025-08-17 10:59:24 +0900

    test: 제외항목 추가

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	// Configure pageExtensions to include md and mdx
 	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 	outputFileTracingExcludes: {
-		"/*": [".pnpm-store/v3/files"],
+		"/*": [".pnpm-store/v3/files", ".next/cache/webpack", ".git/objects/pack"],
 	},
 };
 
